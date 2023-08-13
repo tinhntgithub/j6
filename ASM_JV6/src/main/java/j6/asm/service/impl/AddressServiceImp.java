e2ef50401ab26d97747ec8fa7d7d3875eab64a8a
Lâm Diễm Thuý <110088530+ThuyLam1210@users.noreply.github.com>
2023-08-13 15:06:53 +0700

    [Backend] - Fixbug report Customer

--- ASM_JV6/src/main/java/j6/asm/service/impl/AddressServiceImp.java
+++ ASM_JV6/src/main/java/j6/asm/service/impl/AddressServiceImp.java
@@ -10,39 +10,42 @@ import j6.asm.dao.AddressDAO;
 import j6.asm.entity.Accounts;
 import j6.asm.entity.Address;
 import j6.asm.service.AddressService;
+
 @Service
 public class AddressServiceImp implements AddressService {
 
 	@Autowired
 	AddressDAO dao;
-		@Override
+
+	@Override
 	public Optional<List<Address>> findByUsername(String username) {
 		return dao.findByUsername(username);
 	}
+
 	
 	@Override
 	public List<Address> findAll() {
 		return dao.findAll();
 	}
-	
+
 	@Override
 	public Address findById(Integer id) {
 		return dao.getById(id);
 	}
-	
+
 	@Override
 	public void create(Address entity) {
 		dao.save(entity);
 	}
-	
+
 	@Override
 	public void update(Address entity) {
 		dao.save(entity);
 	}
+
 	@Override
 	public void delete(Integer id) {
 		dao.deleteById(id);
 	}
-
 
 }
--- ASM_JV6/src/main/resources/static/assets/admin/asset/adminjs/report.js
+++ ASM_JV6/src/main/resources/static/assets/admin/asset/adminjs/report.js
@@ -8,7 +8,7 @@ app.controller("report-favorite", function ($scope, $http) {
 
     $scope.reportFavorites = [];
     $scope.url = function (filename) {
-        return`${url5}/${filename}`;
+        return `${url5}/${filename}`;
     }
 
     // Function to initialize DataTables
@@ -22,7 +22,7 @@ app.controller("report-favorite", function ($scope, $http) {
             searching: false,
             paging: false,
             info: false,
-            ordering: false ,// Tắt tính năng sắp xếp'
+            ordering: false,// Tắt tính năng sắp xếp'
             columns: [
                 { data: 'name', title: 'Tên sản phẩm' },
                 { data: 'img', title: 'Hình ảnh' },
@@ -31,13 +31,13 @@ app.controller("report-favorite", function ($scope, $http) {
             ]
         });
     };
-    
-    
+
+
     // Function to load all order data
     $scope.loadAllreportFavorites = function () {
         var url = "http://localhost:8080/rest/reportFavorites";
         $http.get(url).then(function (response) {
-            $scope.reportFavorites = response.data.map(function(item) {
+            $scope.reportFavorites = response.data.map(function (item) {
                 return {
                     name: item[0],
                     img: item[1],
@@ -51,12 +51,12 @@ app.controller("report-favorite", function ($scope, $http) {
             console.log(error);
         });
     };
-    
+
     // Hàm định dạng giá tiền thành chuỗi có dấu phẩy ngăn cách hàng nghìn và hai chữ số sau dấu phẩy
     function formatCurrency(amount) {
         return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(amount);
     }
-    
+
 
     $scope.loadAllreportFavorites();
 });
@@ -64,16 +64,16 @@ app.controller("report-order", function ($scope, $http) {
     $scope.$on('$routeChangeSuccess', function (event, current, previous) {
         $scope.page.setTitle(current.$$route.title || ' Tổng Hợp - Thống Kê | Đơn Hàng');
     });
- var url5 = "http://localhost:8080/uploads/productImg";
+    var url5 = "http://localhost:8080/uploads/productImg";
     $scope.reportOrder = [];
     $scope.url = function (filename) {
-        return`${url5}/${filename}`;
+        return `${url5}/${filename}`;
     }
     // Function to load all order data
     $scope.loadAllOrder = function () {
         var url = "http://localhost:8080/rest/reportOrder";
         $http.get(url).then(function (response) {
-            $scope.reportOrder = response.data.map(function(item, index) {
+            $scope.reportOrder = response.data.map(function (item, index) {
                 return {
                     stt: index + 1,
                     name: item[0],
@@ -88,7 +88,7 @@ app.controller("report-order", function ($scope, $http) {
         });
     };
 
-    $scope.initDataTable1 = function() {
+    $scope.initDataTable1 = function () {
         $('#example1').DataTable({
             dom: 'Bfrtip',
             buttons: [
@@ -101,9 +101,11 @@ app.controller("report-order", function ($scope, $http) {
             columns: [
                 { data: 'stt', title: 'STT' },
                 { data: 'name', title: 'Tên sản phẩm' },
-                { data: 'imgUrl', title: 'Hình ảnh', render: function(data) {
-                    return `<img class="img-fluid" src="${data}" style="width: 64px;height: 64px;">`;
-                } },
+                {
+                    data: 'imgUrl', title: 'Hình ảnh', render: function (data) {
+                        return `<img class="img-fluid" src="${data}" style="width: 64px;height: 64px;">`;
+                    }
+                },
                 { data: 'numberOfSales', title: 'Số lượng bán' }
             ]
         });
@@ -120,87 +122,199 @@ app.controller("report-customer", function ($scope, $http, $filter) {
     $scope.dateNow = new Date();
     $scope.reportCustomer = [];
 
-    // Function to initialize DataTable
-    $scope.initDataTable = function () {
-        $('#example2').DataTable({
-            dom: 'Bfrtip',
-            buttons: [
-                'copy', 'csv', 'excel', 'pdf', 'print'
-            ],
-            searching: false,
-            paging: true,
-            info: false,
-            data: $scope.reportCustomer,
-            columns: [
-                { data: 'name', title: 'Tên Khách Hàng' },
-                { data: 'createDate', title: 'Ngày Đặt Hàng' },
-                { data: 'productCount', title: 'Số Lượng Sản Phẩm' },
-                { data: 'totalSum', title: 'Tổng Tiền' }
-            ]
-        });
-    };
+    
 
     // Function to load all customer data
     $scope.loadAllCustomer = function () {
         var url = `${host}/reportCustomerAll`;
         $http.get(url).then(function (response) {
-            $scope.reportCustomer = response.data.map(function(item) {
-                return {
-                    name: item[0],
-                    createDate: formatDate(new Date(item[1])),
-                    productCount: item[2],
-                    totalSum: formatCurrency(item[3])
-                };
-            });
-            $scope.initDataTable();
+            $scope.reportCustomer = response.data;
+            // $scope.initDataTable();
             console.log(response.data);
         }).catch(function (error) {
             console.log(error);
         });
     };
-// Cập nhật dữ liệu trong DataTable hiện tại
-$scope.updateDataTable = function(newData) {
-    var table = $('#example2').DataTable();
-    table.clear().rows.add(newData).draw();
-};
+    // Cập nhật dữ liệu trong DataTable hiện tại
+    // $scope.updateDataTable = function (newData) {
+    //     var table = $('#example2').DataTable();
+    //     table.clear().rows.add(newData).draw();
+    // };
     // Function to load customer report data based on date range
-    
+
     $scope.dates = function () {
         var dateS = $filter('date')($scope.dateStart, "yyyy-MM-dd");
         var dateE = $filter('date')($scope.dateEnd, "yyyy-MM-dd");
-    
+
         if (!dateS) {
             dateS = $("#start").text();
         }
         if (!dateE) {
-            dateE = $("#end").text();
+            dateE = $("#start").text();
         }
-    
+
         var url = `${host}/reportCustomer?start=${dateS}&end=${dateE}`;
         $http.get(url)
             .then(function (response) {
                 // Success callback
-                $scope.reportCustomer = response.data.map(function(item) {
-                    return {
-                        name: item[0],
-                        createDate: formatDate(new Date(item[1])),
-                        productCount: item[2],
-                        totalSum: formatCurrency(item[3])
-                    };
-                });
-                $scope.updateDataTable($scope.reportCustomer);
+                $scope.reportCustomer = response.data;
                 console.log(response.data);
-                // $scope.initDataTable();
-                
             })
             .catch(function (error) {
                 // Error callback
                 console.log(error);
             });
-    
+
         console.log(dateS, dateE);
     };
+    $scope.printPDF = function () {
+        var headerTable = {
+            table: {
+                headerRows: 1,
+                widths: ['auto', 'auto', 'auto', 'auto'],
+                body: [
+                    ['#', 'Tên Khách Hàng', 'Tổng Đơn Hàng', 'Tổng Tiền']
+                ]
+            }
+        };
     
+        var bodyTable = {
+            table: {
+                widths: ['auto', 'auto', 'auto', 'auto'],
+                body: $scope.reportCustomer.map((item, index) => [
+                    index + 1,
+                    item[0],
+                    item[1],
+                    formatCurrency(item[2])
+                ])
+            }
+        };
+    
+        var docDefinition = {
+            content: [
+                { text: 'Báo cáo danh sách đơn hàng khách hàng', style: 'header' },
+                ' ',
+                { text: 'Ngày in: ' + formatDate(new Date()), alignment: 'right' },
+                ' ',
+                headerTable,
+                bodyTable
+            ],
+            styles: {
+                header: { fontSize: 18, bold: true, alignment: 'center' }
+            }
+        };
+      
+        pdfMake.createPdf(docDefinition).open();
+        //...
+    };
+    
+
+  
+    $scope.exportExcel = function () {
+        var data = [
+            ['BÁO CÁO - THỐNG KÊ THEO KHÁCH HÀNG'], // Header
+            [], // Empty row for spacing
+            ['#', 'Tên Khách Hàng', 'Tổng Đơn Hàng', 'Tổng Tiền'],
+            ...$scope.reportCustomer.map((item, index) => [
+                index + 1,
+                item[0],
+                item[1],
+                formatCurrency(item[2])
+            ])
+        ];
+    
+        var ws = XLSX.utils.aoa_to_sheet(data);
+        var wb = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, 'Danh sách đơn hàng');
+    
+        XLSX.writeFile(wb, 'danh_sach_don_hang.xlsx');
+    };
+    
+    
+    
+    $scope.printDetail = function (username) {
+
+        var url = `${host}/reportPrintCustomer/${username}`;
+
+        $http.get(url)
+            .then(function (response) {
+                var invoiceData = response.data; // Dữ liệu chi tiết hóa đơn từ API
+                var printWindow = window.open('', '_blank');
+                var customerName = invoiceData[0][0];
+                var totalAmount = 0;
+                for (var i = 0; i < invoiceData.length; i++) {
+                    totalAmount += invoiceData[i][2]; // Cột 2 là tổng tiền đơn hàng
+                }
+
+                var content = `
+                <!DOCTYPE html>
+<html>
+<head>
+    <title>Chi tiết hóa đơn khách hàng</title>
+    <!-- Liên kết đến tệp CSS của Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .invoice-container {
+            margin: 0 auto;
+            border: 1px solid #ccc;
+            padding: 20px;
+        }
+        .left-column {
+            text-align: left;
+        }
+        h2 {
+            margin-bottom: 10px;
+        }
+        p {
+            margin: 5px 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="container invoice-container">
+        <h2>Chi tiết hóa đơn khách hàng</h2> <p>Tên khách hàng: ${customerName}</p>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Mã Hóa Đơn</th>
+                   
+                    <th>Tổng Tiền</th>
+                    <th>Ngày Đặt Hàng</th>
+                </tr>
+            </thead>
+            <tbody>
+                ${invoiceData.map(item => `
+                    <tr>
+                        <td>${item[1]}</td>
+                       
+                        <td>${formatCurrency(item[2])}</td>
+                        <td>${formatDate(new Date(item[3]))}</td>
+                    </tr>
+                `).join('')}
+            </tbody>
+        </table>
+        <p>Tổng tiền của tất cả đơn hàng: ${formatCurrency(totalAmount)}</p>
+    </div>
+
+    <!-- Liên kết đến các tệp JavaScript của Bootstrap và jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+                `;
+                console.log("success", response.data)
+                printWindow.document.write(content);
+                printWindow.print();
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
 
     // Helper function to format date
     function formatDate(date) {
@@ -210,7 +324,7 @@ $scope.updateDataTable = function(newData) {
         var day = date.getDate();
         var month = date.getMonth() + 1;
         var year = date.getFullYear();
-        return `${day}/${month}/${year}`;
+        return `${day}-${month}-${year}`;
     }
 
     // Helper function to format currency
